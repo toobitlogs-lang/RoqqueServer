d0a7c5c289f5630fbc4fef39eff86ef9385f2cc7
god130673 <god130673@gmail.com>
2024-12-07 19:55:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "god130673@gmail.com";
-const pass = "zhqtbiluqirmaxbs";
+const pass = "imzrmwhyabkuauuu";
 //novah  16 oct
 
 // Middleware
