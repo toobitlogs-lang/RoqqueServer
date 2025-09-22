cb8aef6ad6bea18dde89e2e4fa0130adbe2294a9
toobitlogs-lang <toobitlogs@gmail.com>
2025-09-22 01:17:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "god130673@gmail.com";
-const pass = "imzrmwhyabkuauuu";
+const pass = "wvimeygpqfiskava";
 //novah  21 JUNE
 
 // Middleware
@@ -173,3 +173,4 @@ app.post("/auth", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
+
